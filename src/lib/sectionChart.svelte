23743dd60e3faa050b99ecd0ebcec65b1f833181
Segundev <jayesegun03@gmail.com>
2024-10-09 12:18:05 +0100

    flourish

--- src/lib/sectionChart.svelte
+++ src/lib/sectionChart.svelte
@@ -1,12 +1,7 @@
 <section>
   <h3>Consumer Price Index</h3>
   <div class="flourish-embed flourish-chart" data-src="visualisation/19721541">
-    <script src="https://public.flourish.studio/resources/embed.js"></script><noscript
-      ><img
-        src="https://public.flourish.studio/visualisation/19721541/thumbnail"
-        width="100%"
-        alt="chart visualization"
-      /></noscript
+    <script src="https://public.flourish.studio/resources/embed.js"></script>
     >
   </div>
 </section>
